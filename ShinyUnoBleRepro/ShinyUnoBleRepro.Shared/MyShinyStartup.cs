9b50b32f8b0d6c4ab0ab207c3eed51ecef81a8b0
Patrick M. Decoster <p.decoster@vogels.com>
2020-09-18 14:13:48 +0200

    Add project files.

---
+++ ShinyUnoBleRepro/ShinyUnoBleRepro.Shared/MyShinyStartup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Shiny;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShinyUnoBleRepro.Shared
+{
+    public class MyShinyStartup : Shiny.ShinyStartup
+    {
+        public override void ConfigureServices(IServiceCollection services)
+        {
+            // for general client functionality
+            services.UseBleClient();
+        }
+    }
+}
